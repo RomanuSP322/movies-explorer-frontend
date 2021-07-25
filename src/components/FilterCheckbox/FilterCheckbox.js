3f9c4fba72a91d329544e95f856f4a584f5c9494
Roman SP <prs@m-cti.ru>
2021-07-25 23:04:51 +0300

    add:  structure

---
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './FilterCheckbox.css';
+
+function FilterCheckbox() {
+  return (
+
+  );
+}
+
+export default FilterCheckbox;
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SearchForm.css';
+
+function SearchForm() {
+  return (
+
+  );
+}
+
+export default SearchForm;
